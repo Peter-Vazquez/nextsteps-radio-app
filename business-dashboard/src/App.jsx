bfefe04d21e66252912e09a14d42375a9ec762f5
Peter Vazquez <peter@NextStepsShow.com>
2026-07-16 11:17:23 -0400

    Load live progress dashboard styles

--- business-dashboard/src/App.jsx
+++ business-dashboard/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PublicDashboard from './PublicDashboard.jsx';
 import OwnerPortal from './OwnerPortal.jsx';
 import './final.css';
+import './progress.css';
 
 export default function App() {
   const path = window.location.pathname.replace(/\/+$/, '') || '/';
