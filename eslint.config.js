ab21d7b30cfa7b19b5030b0fc1461631c3cd43b6
Peter Vazquez <peter@NextStepsShow.com>
2026-08-06 22:55:58 -0400

    Add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+const recommendedRules = js.configs.recommended.rules;
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      'react-hooks': reactHooks
+    },
+    rules: {
+      ...recommendedRules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  },
+  {
+    files: ['*.config.js', 'scripts/**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.nodeBuiltin
+    },
+    rules: recommendedRules
+  }
+];
